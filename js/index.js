f4c42b04e4877f45e0791e22d6e2852ca9e41661
Kim Hyewon <gpdnjs861@naver.com>
2021-11-07 14:50:39 +0900

    header_css_open_js

--- js/index.js
+++ js/index.js
@@ -8,6 +8,15 @@ scrollHandle();
 $(window).on('scroll', function () {
   scrollHandle();
 });
+
+$('#gnb > ul > li > a').on('mouseover focus', function () {
+  var index = $('#gnb > ul > li').index($(this).parent());
+  $(`section:eq(${index})`).addClass('open');
+});
+$('#gnb > ul > li > a').on('mouseleave focusout', function () {
+  $('section').removeClass('open');
+});
+
 function scrollHandle() {
   var scrollAmt = $(window).scrollTop();
   $('#quick-menu').css({ top: `${scrollAmt + 200}px` });
